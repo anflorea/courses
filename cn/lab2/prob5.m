bedb679f56b2f12f425b3fdc13c61803fd45ef71
Andrei Florea <floreandreimihai@gmail.com>
2018-03-14 16:15:16 +0200

    Finished problem 5

--- cn/lab2/prob5.m
+++ cn/lab2/prob5.m
@@ -3,5 +3,14 @@ x = 2 : 2 : 8;
 f = 4 : 4 : 16;
 f(3) = 14;
 
-disp(x);
-disp(f);
+d = zeros(4, 4);
+
+d(:,1) = f';
+
+for j = 2:4
+   for i = 1:4 - j + 1
+        d(i, j) = (d(i + 1, j - 1) - d(i, j - 1)) / (x(i + 1) - x(i));
+   end
+end
+
+disp(d);
