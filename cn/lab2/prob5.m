fa687de10a9222718ef31333666c444c6ce7b794
Andrei Florea <floreandreimihai@gmail.com>
2018-03-14 16:31:22 +0200

    Fixed bug in formula

--- cn/lab2/prob5.m
+++ cn/lab2/prob5.m
@@ -9,7 +9,7 @@ d(:,1) = f';
 
 for j = 2:4
    for i = 1:4 - j + 1
-        d(i, j) = (d(i + 1, j - 1) - d(i, j - 1)) / (x(i + 1) - x(i));
+        d(i, j) = (d(i + 1, j - 1) - d(i, j - 1)) / (x(i + j - 1) - x(i));
    end
 end
 
