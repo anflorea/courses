6ba3d42d2604dcd500c6ba87115adcfb8ac2d72a
Andrei Florea <floreandreimihai@gmail.com>
2018-03-28 17:41:49 +0300

    Fixed prob3 from lab4

--- cn/lab4/prob3.m
+++ cn/lab4/prob3.m
@@ -1,18 +1,41 @@
-x = 1 : 2 : 115;
-x1 = sqrt(x);
-
-m = length(x);
 
 X = 115;
-n=length(x);
-D = x1 - x;
-A(:,1) = f;
+x = [100, 121, 144, 169];
 
-for i = 2 : n
-    for j = 2 : i
-        A(i, j) = (D(j - 1) * A(i, j - 1) - D(i) * A(j - 1, j -1 )) / (x(i) - x(j - 1));
+for i = 1:length(x)
+   for j = 1:length(x)
+       if abs(x(i) - X) < abs(x(j) - X)
+           aux = x(i);
+           x(i) = x(j);
+           x(j) = aux;
+       end
+   end
+end
+
+disp(x);
+
+fx = sqrt(x);
+
+e = 0.001;
+f = zeros(length(x), length(x));
+f(:, 1) = fx';
+
+for i = 2 : length(x)
+    for j = 1 : i
+        f(i, j + 1) = 1 / (x(i) - x(j)) * (f(j, j) * (x(i) - X) - f(i, j) * (x(j) - X));
     end
 end
 
+v = diag(f);
 
-disp(A(n,n));
+ok = 0;
+for i = 3 : length(v)
+    if abs(v(i) - v(i - 1)) <= e
+        ok = 1;
+        fprintf("aproximation of sqrt(%d) is: %f\n", X, v(i));
+    end
+end
+
+if ok == 0
+    fprintf("There are not enough nodes!\n");
+end
